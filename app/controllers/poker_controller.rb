a330e0f3eaf5ddc3e2551016fa5be438976dd998
Felipe <fbarrios.ing.informatico@gmail.com>
2017-11-03 01:30:45 -0300

    ADD: second player

--- app/controllers/poker_controller.rb
+++ app/controllers/poker_controller.rb
@@ -1,7 +1,13 @@
 class PokerController < ApplicationController
   def index
-    @poker   = PokerService.new
-    @hand    = @poker.delear
-    @ranking = @poker.evaluate
+    @poker           = PokerService.new
+    @one_played      = PokerService.new(@poker.token)
+    @one_delear      = @one_played.delear.faces
+    @second_played   = PokerService.new(@poker.token)
+    @second_delear   = @second_played.delear.faces
+    @one_evaluate    = @one_played.evaluate
+    @one_ranking     = @one_played.ranking
+    @second_evaluate = @second_played.evaluate
+    @second_ranking  = @second_played.ranking
   end
 end
---
+++ test/controllers/poker_controller_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PokerControllerTest < ActionDispatch::IntegrationTest
+end
